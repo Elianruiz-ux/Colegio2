d928fcd87b534e064b50322aa29a3345e0c80d0f
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-02-16 21:16:19 -0500

    commit 2
    funciona

--- src/main/java/co/edu/poli/colegio2/estudiante.java
+++ src/main/java/co/edu/poli/colegio2/estudiante.java
@@ -3,5 +3,6 @@ package co.edu.poli.colegio2;
 public class estudiante {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("funciona github xd");
     }
 }

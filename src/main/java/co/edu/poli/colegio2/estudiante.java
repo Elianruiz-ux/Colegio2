1aad8adfc2c68383f193d919bacd451c6484357a
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-03-16 20:27:03 -0500

    commit 3

--- src/main/java/co/edu/poli/colegio2/estudiante.java
+++ src/main/java/co/edu/poli/colegio2/estudiante.java
@@ -1,6 +1,15 @@
 package co.edu.poli.colegio2;
 
 public class estudiante {
+
+   /*
+    public int age;
+    private boolean isMarried;
+    protected float d:
+    double x;
+    char p;
+    long e;
+   */
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("funciona github xd");
